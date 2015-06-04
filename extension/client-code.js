03508652c6d2dc7bab31ccfc107c4fcbe80abe73
Kaizhi Wei <kaizhi@yahoo-inc.com>
2015-06-03 17:01:46 -0700

    update to work with new embedr

---
+++ extension/client-code.js
@@ -0,0 +1,25 @@
+(function (win, doc, fe, script, url) {
+	// If win[fe] exists, we'va already done this work, so do nothing.
+	if (win[fe]) {
+		return;
+	}
+	// if the embed code doesn't exist on the page already
+	// we create a queue
+	win[fe] = {
+		process: function (type) {
+			// push any process calls into the queue
+			(win[fe].q = win[fe].q || []).push(type);
+		}
+	};
+	// create a script element for the
+	// loader script
+	var s = doc.createElement(script),
+		target = doc.getElementsByTagName(script)[0];
+
+	s.async = 1;
+	s.src = url;
+	// add the script to the DOM so that it loads
+	target.parentNode.insertBefore(s, target);
+
+})(window, document, 'FlickrEmbedr', 'script', 'http://flickr-embedr.v1.production.manhattan.bf1.yahoo.com/assets/embedr-loader.js');
+window.FlickrEmbedr.process('all');
--- extension/fi2e.js
+++ extension/fi2e.js
@@ -1,9 +1,15 @@
 //create a script element for the
 //embedder script
-var s = document.createElement('script'),
-	target =document.getElementsByTagName('script')[0];
+// var s = document.createElement('script'),
+// 	target =document.getElementsByTagName('script')[0];
 
-s.async=1;
-s.src='https://widgets.flickr.com/embedr/embedr-global.js';
-//add the script to the DOM so that it loads
-target.parentNode.insertBefore(s,target);
+// s.async=1;
+// s.src='http://flickr-embedr.v1.production.manhattan.bf1.yahoo.com/assets/embedr.js';
+// //add the script to the DOM so that it loads
+// target.parentNode.insertBefore(s,target);
+var s = document.createElement('script');
+s.src = chrome.extension.getURL('client-code.js');
+(document.head||document.documentElement).appendChild(s);
+s.onload = function() {
+    s.parentNode.removeChild(s);
+};
